07cd27e2f7678372f729ee6620539fd75f100cd5
jcpelletier <joel@Jpelletier.com>
2018-04-21 18:49:33 -0400

    bullet fix part 1
    small updates to partially fix collision with bullets

--- JumpandShootManPrototype/Assets/Scripts/EnemyBullet.cs
+++ JumpandShootManPrototype/Assets/Scripts/EnemyBullet.cs
@@ -3,32 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyBullet : MonoBehaviour {
-    public Transform explosionPrefab;
-    // Use this for initialization
-    void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Awake ()
-    {
-        //gameObject.GetComponent<Rigidbody>().velocity = gameObject.transform.forward * 20;
-    }
+    public Transform explosionPrefab; //this is the thing you instantiate when the bullet dies
 
     void OnCollisionEnter(Collision collision)
     {
+        Debug.Log("Enemy Bullet Collision with " + collision.transform.name);
+        Debug.Log("Bullet hit");
         ContactPoint contact = collision.contacts[0];
         Quaternion rot = Quaternion.FromToRotation(Vector3.up, contact.normal);
         Vector3 pos = contact.point;
-        //Instantiate(explosionPrefab, pos, rot);
-
-        if (collision.gameObject.name == "Player")
+        Instantiate(explosionPrefab, pos, rot);
+        Debug.Log("Check before tag check");
+        if (collision.gameObject.tag =="Player")
         {
             Debug.Log("Bullet hit player");
             collision.gameObject.GetComponent<PlayerStats>().DecrementHealth();
         }
 
-        gameObject.SetActive(false);
+        Destroy(gameObject);
         Debug.Log("EnemyBullet Collided with not player");
     }
+
+   
 }
