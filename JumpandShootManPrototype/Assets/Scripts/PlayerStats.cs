53a2181e1351f9a5b278f1c5be744d3f7cad0a97
jcpelletier <joel@Jpelletier.com>
2018-06-02 10:12:29 -0400

    Crossbow and network menu
    switched buster with a crossbow model with bolts, added a network menu

--- JumpandShootManPrototype/Assets/Scripts/PlayerStats.cs
+++ JumpandShootManPrototype/Assets/Scripts/PlayerStats.cs
@@ -59,25 +59,37 @@ public class PlayerStats : NetworkBehaviour
     //Sound
     public AudioSource hitSound;
     public AudioSource menuthudSound;
-
-    public int health;
-    public int healthMax;
+    
+    //Network sync these values
+    [SyncVar]
+    public float health;
+    [SyncVar]
+    public float healthMax;
+    [SyncVar]
     public float mana;
+    [SyncVar]
     public int manaMax;
+    [SyncVar]
     public float energy;
+    [SyncVar]
     public int energyMax;
+    [SyncVar]
     public int damage;
+    [SyncVar]
     public float manaRegen;
+    [SyncVar]
     public float energyRegen;
 
     //Player references
     //private GameObject playerController; 
     private Rigidbody bod;
     public Camera cam;
+    private Vector3 startPosition;
 
     // Use this for initialization
     void Start ()
-    {   
+    {
+        startPosition = new Vector3(0, 7, 0);
         StartCoroutine("rollStats");
         if (!isLocalPlayer)
         {
@@ -88,9 +100,7 @@ public class PlayerStats : NetworkBehaviour
 
     void Update()
     {
-        if (!isLocalPlayer) {
-            return;
-        }
+
         healthSlider.value = health;
         manaSlider.value = mana;
         energySlider.value = energy;
@@ -102,11 +112,13 @@ public class PlayerStats : NetworkBehaviour
         if (mana > manaMax)
         {
             mana = manaMax;
+            manaLabelText.text = mana.ToString();
         }
 
         if (energy > energyMax)
         {
             energy = energyMax;
+            energyLabelText.text = energy.ToString();
         }
 
         if (isAirControl == true)
@@ -116,36 +128,81 @@ public class PlayerStats : NetworkBehaviour
             gameObject.GetComponent<RigidbodyFirstPersonController>().advancedSettings.airControl = false;
         }
 
+        if (health <= 0)
+        {
+            CmdDeath();
+        }
 
     }
 
     void FixedUpdate()
     {
-        
         PassiveRegen();
+    }
+
+    [Command]
+    public void CmdDeath()
+    {
+       if (isLocalPlayer)
+       {
+            Debug.Log("death called");
+            //do death stuff?
+            gameObject.transform.position = startPosition;
+            health = healthMax;
+            healthLabelText.text = health.ToString();
+       }
+        
     }
 
     public void PassiveRegen()
     {
         energy = energy + 0.02f * resolve;
+        if (energy > energyMax)
+        {
+            energy = energyMax;
+            energyLabelText.text = energy.ToString();
+        }
         mana = mana + 0.02f * willpower;
+        if (mana > manaMax)
+        {
+            mana = manaMax;
+            manaLabelText.text = mana.ToString();
+        }
+        health = health + 0.02f * resolve;
+        if (health > healthMax)
+        {
+            health = healthMax;
+            healthLabelText.text = health.ToString();
+        }
     }
 
     public void DecrementMana()
     {
         mana = mana - 20;
+        manaLabelText.text = mana.ToString();
     }
 
     public void DecrementEnergy(float a)
     {
         energy = energy - a;
+        energyLabelText.text = energy.ToString();
     }
 
-    public void DecrementHealth()
+    [Command]
+    public void CmdDecrementHealth()
     {
+
         Debug.Log("Reducing health");
         hitSound.Play();
-        health = health - 25;
+        Debug.Log("Health was " + health.ToString());
+        health = health - 35;
+        Debug.Log("Health is now " + health.ToString());
+        healthLabelText.text = health.ToString();
+        if (health <= 0)
+        {
+            CmdDeath();
+        }
+
     }
 
     IEnumerator rollStats()
@@ -169,7 +226,6 @@ public class PlayerStats : NetworkBehaviour
          
         fpsHud.SetActive(false);
 
-
         //roll stats
         bravery = 10 + Random.Range(0, 8);
         cunning = 10 + Random.Range(0, 8);
@@ -192,8 +248,9 @@ public class PlayerStats : NetworkBehaviour
         energyRegen = cunning / 10;
         manaRegen = willpower / 10;
 
-        //Set movement ability
+        //Set movement ability 
         canjetpack = true;
+
         //Set max fuel Displays
         healthLabelText.text = healthMax.ToString();
         energyLabelText.text = energyMax.ToString();
